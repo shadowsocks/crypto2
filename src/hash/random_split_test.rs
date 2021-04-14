5078875bfb7a6493c326c50af76b5cec256ba5d1
DuckSoft <realducksoft@gmail.com>
2021-04-14 18:44:27 +0800

    boom boom boom boom
    more boom, more fun

--- src/hash/random_split_test.rs
+++ src/hash/random_split_test.rs
@@ -1,36 +1,35 @@
 pub mod test {
+    use super::super::{Md2, Md4, Md5, Sha1, Sha224, Sha256, Sha384, Sha512, Sm3};
     use rand::{thread_rng, Rng, RngCore};
-    use super::super::{md2::Md2, md4::Md4, md5::Md5, sha1::Sha1, sm3::Sm3};
-    use crate::hash::{Sha256, Sha512};
 
     macro_rules! random_split_test {
-    ($x:ident) => {
-        for stream_size in 2..1024 {
-            let mut buffer = vec![0u8; stream_size];
-            thread_rng().fill_bytes(&mut buffer);
+        ($x:ident) => {
+            for stream_size in 2..1024 {
+                let mut buffer = vec![0u8; stream_size];
+                thread_rng().fill_bytes(&mut buffer);
 
-            let h1 = $x::oneshot(&buffer);
+                let h1 = $x::oneshot(&buffer);
 
-            let break_size = thread_rng().gen_range(1..stream_size);
-            let left = &buffer[..break_size];
-            let right = &buffer[break_size..];
+                let break_size = thread_rng().gen_range(1..stream_size);
+                let left = &buffer[..break_size];
+                let right = &buffer[break_size..];
 
-            let mut ctx = $x::new();
-            ctx.update(left);
-            ctx.update(right);
-            let h2 = ctx.finalize();
+                let mut ctx = $x::new();
+                ctx.update(left);
+                ctx.update(right);
+                let h2 = ctx.finalize();
 
-            debug_assert_eq!(
-                h1,
-                h2,
-                "expect same result when a stream with size {:} is broken into {:} and {:}",
-                stream_size,
-                break_size,
-                stream_size - break_size
-            );
-        }
-    };
-}
+                debug_assert_eq!(
+                    h1,
+                    h2,
+                    "expect same result when a stream with size {:} is broken into {:} and {:}",
+                    stream_size,
+                    break_size,
+                    stream_size - break_size
+                );
+            }
+        };
+    }
 
     #[test]
     fn random_split_test_md2() {
@@ -58,8 +57,18 @@ pub mod test {
     }
 
     #[test]
+    fn random_split_test_sha224() {
+        random_split_test!(Sha224);
+    }
+
+    #[test]
     fn random_split_test_sha256() {
         random_split_test!(Sha256);
+    }
+
+    #[test]
+    fn random_split_test_sha384() {
+        random_split_test!(Sha384);
     }
 
     #[test]
